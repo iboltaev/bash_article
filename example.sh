551be62ef9f15fb185500c842c74f63f4ccda638
Ilya Boltaev <boltaev@gmail.com>
2017-12-02 13:25:47 +0300

    Update example.sh

--- example.sh
+++ example.sh
@@ -1,35 +1,29 @@
-# reports - tables
-grep -r "\"table\":" projects/*/reports/* | awk '{print $1 " " $3}' | sed 's/[\r\n",:]//g' | grep "\s[A-Za-z0-9]" | sort -k 1b,1 | uniq > report_tables
+# reports - tables                                                                                                                     
+grep -r "\"table\":" projects/*/reports/* | sed 's/:/ /g' | awk '{print $1 " " $3}' | sed 's/[\r\n",:]//g' | grep "\s[A-Za-z0-9]" | sort -k 1b,1 | uniq > report_tables
 
-# reports - dates
-grep -r "\"created_date\":" projects/*/reports/* | awk '{print $1 " " $3"T"$4}' | sed 's/[\r\n\t":,]//g' | sort -k 1b,1 | uniq > report_dates
+# reports - dates                                                                                                                      
+grep -r "\"created_date\":" projects/*/reports/* | sed 's/:/ /g' | awk '{print $1 " " $3"T"$4":"$5":"$6}' | sed 's/[\r\n\t",]//g' | sort -k 1b,1 | uniq > report_dates
 
-# reports - tables & dates
-join report_tables report_dates > report_table_date
+# reports - tables & dates                                                                                                             
+join report_tables report_dates | awk '{print $1"#"$2 " " $3}' > report_table_date
 
-# reports - projects & runs
-cat report_tables | awk '{print $1}' | sort | uniq | while read line; do re1=".*/projects/([^/]*)/reports/.*"; proj=""; if [[ $line =~ $re1 ]]; then proj=${BASH_REMATCH[1]} ; fi; re2=".*/([^/]*)/[^/]*$"; run=""; if [[ $line =~ $re2 ]]; then run=${BASH_REMATCH[1]} ;fi; echo $line $proj:$run; done | sort -k 1b,1 | uniq > report_project_run
+# schema - configs                                                                                                                     
+grep -r "schema\":" ~/projects/*/conf/* | sed 's/:/ /g' | awk '{print $3 " " $1}' | sed 's/[\r\n":,]//g' | sort -k 1b,1 | uniq > schema_configs
 
-# configs - schemas
-grep -r "schema\":" projects/*/conf/* | awk '{print $1 " " $3}' | sed 's/[\r\n\t":,]//g' | sort -k 1b,1 | uniq > config_schemas
+# config - schemas                                                                                                                     
+cat schema_configs | awk '{print $2 " " $1}' | sort -k 1b,1 | uniq > config_schemas
 
-# configs - projects & runs
-cat config_schemas | awk '{print $1}' | sort | uniq | while read line; do re1=".*/<projects>/([^/]*)/conf/.*"; proj=""; if [[ $line =~ $re1 ]]; then proj=${BASH_REMATCH[1]} ; fi; re2=".*/([^/\.]*)\.[^\.]*$"; run=""; if [[ $line =~ $re2 ]]; then run=${BASH_REMATCH[1]} ;fi; echo $line $proj:$run; done | sort -k 1b,1 | uniq > config_project
+# configs - reports                                                                                                                    
+cat schema_configs | awk '{print $2}' | sort | uniq | grep ".conf$" | while read line; do re="^(.*)/conf/.*$"; if [[ $line =~ $re ]]; then pdir=${BASH_REMATCH[1]}/reports; re2=".*/([^/\.]*)\..*"; if [[ $line =~ $re2 ]]; then run=${BASH_REMATCH[1]}; reps=$(find $pdir/$run -name *.json); for r in $reps; do echo $line $r ; done ; fi ; fi ;done | grep -v run_log | sort -k 1b,1 > config_reports
 
-# projects & runs - schemas
-join config_project config_schemas | awk '{print $2 " " $3}' | sort --version-sort | uniq > project_run_schemas
+# schemas - tables                                                                                                                     
+cat schema_configs | awk '{print $1}' | sort | uniq | grep "[A-Za-z0-9]" | sed 's/^/path_in_hive/g' | sed 's/$/\.db/g' | xargs -n1 -I dr hdfs dfs -ls dr | sed 's/\// /g' | sed 's/\.db//g' | awk '{print $12 " " $13 " " $6"T"$7}' | sort -k 1b,1 | uniq > schema_tables
 
-# schemas - tables
-cat config_schemas | awk '{print $2}' | sort | uniq | grep "[A-Za-z0-9]" | sed 's/^/<path_in_hdfs>/g' | sed 's/$/\.db/g' | xargs -n1 -I dr hdfs dfs -ls dr | sed 's/\// /g' | sed 's/\.db//g' | awk '{print $12 " " $13 " " $6"T"$7}' | sort -k 1b,1 | uniq > schema_tables
+# configs - tables                                                                                                                     
+join schema_configs schema_tables | awk '{print $2 " " $3 " " $4}' | sort -k 1b,1 | uniq > config_tables
 
-# schemas - projects & runs
-cat project_run_schemas | awk '{print $2 " " $1}' | sort -k 1b,1 > schema_projects
-
-# projects & runs - tables
-join schema_projects schema_tables | awk '{print $2 "#" $3 " " $4}' | sort -k 1b,1 | uniq > project_run_table_date
-
-# projects & runs & tables - report date
-join report_project_run report_table_date | awk '{print $2"#"$3 " " $1 " " $4}' | sort -k 1b,1 > project_run_report_date
+# reports - tables hive dates
+join config_reports config_tables | awk '{print $2"#"$3 " " $4}' | sort -k 1b,1 > report_table_hive_dates
 
 # final!
-join project_run_report_date project_run_table_date | awk '{ if ($3<$4) print $2 }' | sort | uniq > outdated_reports
+join report_table_date report_table_hive_dates | sed 's/#/ /g' | awk '{if ($3<$4) print $1}' | sort | uniq > outdated_reports
